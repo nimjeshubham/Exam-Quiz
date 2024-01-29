1ecb5d52ff3dd438f076b164d9db51b63b37412a
shubham Kumar Nimje <shubbham13@gmail.com>
2024-01-29 14:21:31 +0530

    Initial commit

---
+++ exam-Quiz/src/main/java/com/school/pack/ExamQuizApplication.java
@@ -0,0 +1,13 @@
+package com.school.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuizApplication.class, args);
+	}
+
+}
